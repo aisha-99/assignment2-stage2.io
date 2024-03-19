c50d225256e2d3698c085c8766e4c1fac0350076
aisha-99 <123507252+aisha-99@users.noreply.github.com>
2024-03-19 12:13:01 -0400

    innit

--- js/script.js
+++ js/script.js
@@ -37,39 +37,39 @@ client.getEntries({content_type: 'assignment2',}).then(function (entries) {
         movieDiv.appendChild(linkToDetails);
         allMovies.appendChild(movieDiv);
 
-        // Description
-        console.log(entry.fields.description);
-        var description = document.createElement('p');
-        description.innerHTML = entry.fields.description;
-        movieDiv.append(description);
-        console.log(entry);
+        // // Description
+        // console.log(entry.fields.description);
+        // var description = document.createElement('p');
+        // description.innerHTML = entry.fields.description;
+        // movieDiv.append(description);
+        // console.log(entry);
 
-        // Gallery Images
-        var galleryImages = document.createElement('div');
-        galleryImages.classList.add('gallery-images');
-        movieDiv.append(galleryImages);
-        entry.fields.galleryImages.forEach(function(image){
-            var galleryImage = document.createElement('img');
-            galleryImage.src = image.fields.file.url;
-            galleryImages.append(galleryImage);
-        })
-        if (entry.fields.galleryImages){
-            console.log(entry.fields.galleryImages);
-        }
+        // // Gallery Images
+        // var galleryImages = document.createElement('div');
+        // galleryImages.classList.add('gallery-images');
+        // movieDiv.append(galleryImages);
+        // entry.fields.galleryImages.forEach(function(image){
+        //     var galleryImage = document.createElement('img');
+        //     galleryImage.src = image.fields.file.url;
+        //     galleryImages.append(galleryImage);
+        // })
+        // if (entry.fields.galleryImages){
+        //     console.log(entry.fields.galleryImages);
+        // }
 
-        // Film Director
-        console.log(entry.fields.filmDirector);
-        var filmDirector = document.createElement('h4');
-        filmDirector.innerHTML = entry.fields.filmDirector;
-        movieDiv.append(filmDirector);
+        // // Film Director
+        // console.log(entry.fields.filmDirector);
+        // var filmDirector = document.createElement('h4');
+        // filmDirector.innerHTML = entry.fields.filmDirector;
+        // movieDiv.append(filmDirector);
 
-        // Trailer Link
-        console.log(entry.fields.trailerLink);
-        var trailerLink = document.createElement('a');
-        trailerLink.innerHTML = "link to " + entry.fields.trailerLink;
-        trailerLink.href = entry.fields.trailerLink;
-        movieDiv.append(trailerLink);
-        // allMovies.append(movieDiv);
+        // // Trailer Link
+        // console.log(entry.fields.trailerLink);
+        // var trailerLink = document.createElement('a');
+        // trailerLink.innerHTML = "link to " + entry.fields.trailerLink;
+        // trailerLink.href = entry.fields.trailerLink;
+        // movieDiv.append(trailerLink);
+        // // allMovies.append(movieDiv);
         
 
 
